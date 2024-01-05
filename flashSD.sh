688f269cd6d16b576533b963460b2cec739f25ca
ImogenWren <imogen.wren91@gmail.com>
2024-01-05 14:29:05 +0000

    updated flashsd

--- flashSD.sh
+++ flashSD.sh
@@ -4,6 +4,6 @@
 # Flash  sd-card.img  diskimage to SD card drive named in script call
 
 
-echo "Flashing sd-card.img to /dev/"$1
-#sudo dd if=sd-card.img of=/dev/$1
-sudo dd if=sd-card.img of=/dev/sda bs=16M
+echo "Flashing sd-card.img to /dev/sda"$1
+#sudo dd if=sd-card.img of=/dev/sda
+sudo dd if=sd-card.img of=/dev/sda bs=16M status=progress
